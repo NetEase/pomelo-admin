c8e787fae7ec8341fa8c16ecd2b03567d5d30b41
fantasyni <fantasyni@163.com>
2013-04-28 16:47:42 +0800

    add admin module handle module

--- index.js
+++ index.js
@@ -6,11 +6,14 @@ module.exports.createMonitorConsole = consoleService.createMonitorConsole;
 module.exports.adminClient = require('./lib/client/client');
 
 exports.modules = {};
-fs.readdirSync(__dirname + '/lib/modules').forEach(function(filename){
-  if (/\.js$/.test(filename)) {
-    var name = filename.substr(0, filename.lastIndexOf('.'));
-    exports.modules.__defineGetter__(name, function(){
-      return require('./lib/modules/' + name);
-    });
-  }
+fs.readdirSync(__dirname + '/lib/modules').forEach(function(filename) {
+	if (/\.js$/.test(filename)) {
+		var name = filename.substr(0, filename.lastIndexOf('.'));
+		var _module = require('./lib/modules/' + name);
+		if (!_module.moduleError) {
+			exports.modules.__defineGetter__(name, function() {
+				return _module;
+			});
+		}
+	}
 });
